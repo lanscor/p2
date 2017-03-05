c5b18084ecfdca84a73b3a32e7e755238ae5186a
jdubois52 <jdubois3@wisc.edu>
2017-03-05 17:16:10 -0600

    Update GameApp.java

--- GameApp.java
+++ GameApp.java
@@ -4,9 +4,16 @@
 // FILE:             Scoreboard.java
 //
 // TEAM:    The Brogrammers, 07
-// Authors: Jonas Klare, Bryan Watson
-// Author1: Jonas Klare, klare@wisc.edu, klare, 001
-// Author2: Bryan Watson, bmwatson2@wisc.edu, bmwatson2, 001
+// Authors: Zexing Li (Richard), Bryan Watson, Changhao Sun, Jonas Klare, Mason Gomm, Joshua DuBois,
+//
+// Author1: Zexing Li (Richard), zli674@wisc.edu, zexing, lec001
+// Author2: Bryan Watson, bmwatson2@wisc.edu, bmwatson2, lec001
+// Author3: Joshua DuBois, jdubois3@wisc.edu, jdubois3, lec001
+// Author4: Changhao Sun, csun78@wisc.edu, csun78, lec001
+// Author5: Mason Gomm, Mgomm@wisc.edu, mgomm, lec001
+// Author6: Jonas Klare, klare@wisc.edu, klare, Lec001
+//
+
 //
 // ---------------- OTHER ASSISTANCE CREDITS 
 // Persons: N/a
