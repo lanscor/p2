ec5d757f6e74cd03872acaa15be82838c697d646
jdubois52 <jdubois3@wisc.edu>
2017-03-05 17:15:29 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,9 +3,15 @@
 // PROJECT:          GameApp
 // FILE:             Game.java
 //
-// TEAM:    The Brogrammers, 07
-// Authors: 
-// Author1: 
+// Authors: Zexing Li (Richard), Bryan Watson, Changhao Sun, Jonas Klare, Mason Gomm, Joshua DuBois,
+//
+// Author1: Zexing Li (Richard), zli674@wisc.edu, zexing, lec001
+// Author2: Bryan Watson, bmwatson2@wisc.edu, bmwatson2, lec001
+// Author3: Joshua DuBois, jdubois3@wisc.edu, jdubois3, lec001
+// Author4: Changhao Sun, csun78@wisc.edu, csun78, lec001
+// Author5: Mason Gomm, Mgomm@wisc.edu, mgomm, lec001
+// Author6: Jonas Klare, klare@wisc.edu, klare, Lec001
+//
 //
 // ---------------- OTHER ASSISTANCE CREDITS 
 // Persons: N/a
